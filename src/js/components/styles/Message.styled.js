d6e88abee45bf6d43e103cafab98764415b79696
unknown <amsstudio.e@gmail.com>
2022-04-20 10:32:22 -0700

    Chore: Save work

--- src/js/components/styles/Message.styled.js
+++ src/js/components/styles/Message.styled.js
@@ -16,7 +16,6 @@ padding: 0 var(--m-padding);
 color: #333;
 word-break: break-all;
 z-index: 2;
-
 box-shadow:${({ className }) => className === 'end' ? '' : '0 0px 4px rgba(169, 169, 169, 0.16), 0 0px 4px rgb(78 78 78 / 25%)'};
 background-color:${({ type }) => type === 'type-0' ? '#cae0ff;' : '#fff'};
 float:${({ type }) => type === 'type-0' ? 'right;' : '#left'};
